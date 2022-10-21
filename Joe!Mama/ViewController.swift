630da3d81f0766b111192d0d2a964f02e3f1608a
Samir Pandey <samee.pan1994@gmail.com>
2022-10-20 20:28:18 -0700

    Initial Commit

---
+++ Joe!Mama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joe!Mama
+//
+//  Created by Samir Pandey on 10/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
